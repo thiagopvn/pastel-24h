c2d8d6d654dbb18225b241ef425eb5ddeecbde41
Your Name <your.email@example.com>
2025-05-30 21:20:02 -0300

    L

--- src/js/admin-controller.js
+++ src/js/admin-controller.js
@@ -1914,7 +1914,6 @@ window.CashControlManager = CashControlManager;
     } else {
         // Criar nova instância do CashControlManager
         const cashControlManager = new CashControlManager();
-        window.cashControlManager = cashControlManager;
         await cashControlManager.load();
     }
     break;
